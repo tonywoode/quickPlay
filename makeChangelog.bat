00991b00a0dfb566af0af361feb2702628ae708b
tony woode <tonywoodecode@gmail.com>
2015-05-30 20:37:09 +0100

    changelog echoes to project log and placeholds for web

--- makeChangelog.bat
+++ makeChangelog.bat
@@ -1,7 +1,16 @@
 set LASTVERSION=v4.1.0
 set THISVERSION=v4.1.1
-git log --oneline --no-decorate --no-merges --pretty=tformat:"* %%s"  %LASTVERSION%..%THISVERSION% > changelogLF
+
+copy qp\changelog.txt changelog.txt
+
+git log --oneline --no-decorate --no-merges --pretty=tformat:"* %%s"  %LASTVERSION%..HEAD > changelogLF
+echo."Change the changelog at this point please - its in this scripts dir called changelogLF"
+pause
 echo Quickplay %THISVERSION% > changewin.txt
 echo. >> changeWin.txt
-type changelogLF | FIND "" /V >> changeWin.txt
-echo. >> changeWin.txt
+type changelogLF | FIND "" /V >> changewin.txt
+echo. >> changewin.txt
+
+copy /b changewin.txt+changelog.txt qp\changelog.txt
+del changelog.txt
+del changeWin.txt

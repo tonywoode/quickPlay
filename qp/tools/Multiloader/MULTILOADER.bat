0de52e621b5a2af8f2280c6a701e5730500de89f
tony woode <tonywoodecode@gmail.com>
2016-08-20 21:21:47 +0100

    multiloader: fixed bug in runner
    to run without b2e vars we'd need this check,
    however this variable was bombing out if not quoted
    quoting it produces double quotes, but all we really
    want is truthy

--- qp/tools/Multiloader/MULTILOADER.bat
+++ qp/tools/Multiloader/MULTILOADER.bat
@@ -9,5 +9,6 @@ SETLOCAL
 
 if (%b2eprogrampathname%)==() (set _SCRIPTDIR="%~dp0") else (set _SCRIPTDIR=%b2eprogrampathname%)
 pushd %_SCRIPTDIR%
+if ("%b2eargs%")==() (set _PARAMS=%*) else (set _PARAMS=%b2eargs%)
 %_SCRIPTDIR%MULTILOADER_imp.bat %b2eargs%
 popd

362969adef2ecac56c9507dd88eb25036decbcd6
tony woode <tonywoodecode@gmail.com>
2016-08-26 19:21:00 +0100

    made daemon tools drive for zip-mount configurable
    searched DT forums (yet again) for working command line
    for get_letter/get_count. No go. So go back to previous
    ini drive letter setting
    
    however, the point of this setting has now changed. DT
    don't let you specify drive letter so we can't use it for
    that anymore (must just specify drive 0 for mounting), instead
    I use it when looping through for mountable file. but now we should
    be able to combine this with the other mounting paths

--- qp/tools/Multiloader/MULTILOADER_imp.bat
+++ qp/tools/Multiloader/MULTILOADER_imp.bat
@@ -31,6 +31,8 @@ IF EXIST %_SCRIPTDIR%Multiloader.ini (set _INIFILE=%_SCRIPTDIR%Multiloader.ini)
 :: You must set these in the ini file: where to extract to, which drive letter for daemon tools ,and which for daemon's zip support
 for /f "tokens=2* delims==" %%H in ('find "TEMPDIR=" ^< %_INIFILE%') do (set TEMPDIR=%%H)
 if (%TEMPDIR%)==() (for /D %%I IN (%1) DO SET _TEMPDIR=%%~dpnsN\) else (for /D %%I IN (%1) DO SET _TEMPDIR=%TEMPDIR%)
+for /f "tokens=2* delims==" %%J in ('find "DAEMON_DRIVE=" ^< %_INIFILE%') do (set DAEMON_DRIVE=%%J)
+if (%DAEMON_DRIVE%)==() (SET _DAEMON_DRIVE=K) else (SET _DAEMON_DRIVE=%DAEMON_DRIVE%)
 for /f "tokens=2* delims==" %%N in ('find "CLEANTEMP=" ^< %_INIFILE%') do (set _CLEANTEMP=%%N)
 
 ::-------------------------------------------------------------------------------------
@@ -142,10 +144,7 @@ goto FINISH
 :WINMOUNT
 %_DT% -mount SCSI, 0, "%_ROMNAME%"
 set _WINMOUNTING=YES
-
-FOR /R K:\ %%Y IN (*.pdi *.isz *.bwt *.b6t *.b5t *.nrg *.iso *.img *.cdi *.mdx *.mds *.ccd *.bin *.cue *.gcm *.gdi) DO set _ROMNAME="%%~sY"
-echo %_ROMNAME%
-pause
+FOR /R %_DAEMON_DRIVE%:\ %%Y IN (*.pdi *.isz *.bwt *.b6t *.b5t *.nrg *.iso *.img *.cdi *.mdx *.mds *.ccd *.bin *.cue *.gcm *.gdi) DO set _ROMNAME="%%~sY"
 goto LOAD
 
 :UNZIP

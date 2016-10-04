346ddf0f954cc6977d1e6f3e780ad1c79b222cba
tony woode <tonywoodecode@gmail.com>
2016-10-04 08:37:00 +0100

    second stage of pascal string algorithm

--- devTools/convertPascalStrings.js
+++ devTools/convertPascalStrings.js
@@ -1,18 +1,29 @@
 #!/usr/bin/env node
-// stackoverflow.com/a/33920309 - this is for ascii only, 2-digithexcodes....
+"use strict"
+/* stackoverflow.com/a/33920309 - this is for ascii only, 2-digithexcodes....
+ *  pass it a space as delim should you ever need space separated hexwords
+ */
+
 String.prototype.hexEncodedelim = function(delim){
-    return this.split("").map(function(c){
-      return ("0" + c.charCodeAt(0).toString(16)).slice(-2)
-    }).join(delim || "")
+  return this.split("").map(function(c){
+    return ("0" + c.charCodeAt(0).toString(16)).slice(-2)
+  }).join(delim || "")
 }
 
 
 const
-  fs = require('fs')
-  stream = process.argv[2]
-  answer = stream.hexEncodedelim()
-  console.log("answer is " + answer)
-  return answer
+  fs = require('fs'),
+  stream = process.argv[2],
+  //first convert to hex
+  hexxed = stream.hexEncodedelim().toUpperCase(),
+  //then add a CRLF
+  hexxedAndReturned = hexxed + "0D0A",
+  //next get the number of characters in the string you just made, rem length is a prop not function§
+  numChars = hexxedAndReturned.length
+  console.log(numChars)
+
+  console.log("answer is " + hexxedAndReturned)
+  //return hexxed
 
 
 

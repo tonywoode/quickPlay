b1077c1ce56ecf5676071fe5ec4f81207a469569
tony woode <tonywoodecode@gmail.com>
2016-10-04 19:11:19 +0100

    pad digits

--- devTools/convertPascalStrings.js
+++ devTools/convertPascalStrings.js
@@ -12,12 +12,12 @@ String.prototype.hexEncodedelim = function(delim){
 
 /*
  * stackoverflow/a/57807 - 2's complement means we'll get negative numbers over a certain size
- * we prob also need to pad single numerals
+ * need to pad single numerals also
  */
-function decimalToHex(d, padding) {
+function decimalToHex(d, minLength) {
   var hex = Number(d).toString(16)
-  padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding
-  while (hex.length < padding ) {
+  minLength = typeof (minLength) === "undefined" || minLength === null ? minLength = 2 : minLength
+  while (hex.length < minLength ) {
     hex = "0" + hex
   }
   return hex
@@ -30,14 +30,19 @@ const
   hexxed = string.hexEncodedelim().toUpperCase(),
   //but our pascal strings (and their computed length) include a CRLF on the end
   hexxedAndReturned = hexxed + "0D0A",
-  lengthOf0D0A = 2, //when unhexed, that is...
-  stringLength = (string.length + lengthOf0D0A).toString().length,
+  lengthOf0D0A = 2 //when unhexed, that is...
+
+let stringLength = string.length + lengthOf0D0A
+
+const 
+  digitsLength = stringLength.toString().length,
   hexnum = decimalToHex(stringLength),
   lengthOfAByte = 8,
-  endPaddingNeeded = lengthOfAByte - stringLength
+  endPaddingNeeded = lengthOfAByte - digitsLength
   console.log("padding needed is " + endPaddingNeeded)
-  
-  
+  while (stringLength.toString().length < lengthOfAByte) { stringLength = stringLength + "0" } 
+  console.log(stringLength)
+
   console.log("answer is " + hexxedAndReturned)
   //return hexxed
 

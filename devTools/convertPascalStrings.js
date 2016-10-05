08bb9454f6ab1947445d56ecbcf6a137c526a42e
tony woode <tonywoodecode@gmail.com>
2016-10-05 08:34:05 +0100

    complete algorithm (corerected hex number)
    i was encoding the decimal of the size of the
    string, rather than the hex, and not padding
    the initial number correctly in the case of single digits

--- devTools/convertPascalStrings.js
+++ devTools/convertPascalStrings.js
@@ -1,50 +1,62 @@
 #!/usr/bin/env node
 "use strict"
+
+const fs = require('fs')
+
 /* stackoverflow.com/a/33920309 - this is for ascii only, 2-digithexcodes....
  *  pass it a space as delim should you ever need space separated hexwords
  */
 
-String.prototype.hexEncodedelim = function(delim){
+String.prototype.showAsHex = function(delim){
   return this.split("").map(function(c){
     return ("0" + c.charCodeAt(0).toString(16)).slice(-2)
   }).join(delim || "")
 }
 
 /*
- * stackoverflow/a/57807 - 2's complement means we'll get negative numbers over a certain size
+ * stackoverflow/a/57807 - 2's complement so we'll get negative numbers over a 
+ * certain size, but since these are file paths its irrelevant
  * need to pad single numerals also
  */
 function decimalToHex(d, minLength) {
-  var hex = Number(d).toString(16)
-  minLength = typeof (minLength) === "undefined" || minLength === null ? minLength = 2 : minLength
+  let  hex = Number(d).toString(16)
+  minLength =  (
+    typeof (minLength) === "undefined" || minLength === null ? 
+      minLength = 2 : minLength
+  )
   while (hex.length < minLength ) {
     hex = "0" + hex
   }
   return hex
 }
 
+// first convert the string to hex and end with win-style termination
 const
-  fs = require('fs'),
   string = process.argv[2],
-  //first convert to hex
-  hexxed = string.hexEncodedelim().toUpperCase(),
-  //but our pascal strings (and their computed length) include a CRLF on the end
-  hexxedAndReturned = hexxed + "0D0A",
-  lengthOf0D0A = 2 //when unhexed, that is...
+  hexxed = string.showAsHex().toUpperCase(),
+  terminator = '0D0A',  ///our pascal strings (and their computed length) include a CRLF on the end
+  lengthOf0D0A = 2, //when unhexed (length here always means of the string representations)
+  hexxedAndReturned = hexxed + terminator
 
-let stringLength = string.length + lengthOf0D0A
+// now we need to convert the string length to hex and pad it to fit a byte
+//   so when we compute length here, we're talking about the number of chars the hex takes up
+const stringSize = string.length + lengthOf0D0A
+
+let 
+  hexnum = decimalToHex(stringSize)
 
 const 
-  digitsLength = stringLength.toString().length,
-  hexnum = decimalToHex(stringLength),
   lengthOfAByte = 8,
+  digitsLength = hexnum.toString().length,
   endPaddingNeeded = lengthOfAByte - digitsLength
-  console.log("padding needed is " + endPaddingNeeded)
-  while (stringLength.toString().length < lengthOfAByte) { stringLength = stringLength + "0" } 
-  console.log(stringLength)
+  while (hexnum.toString().length < lengthOfAByte) { 
+    hexnum = hexnum + "0" 
+  } 
+  
+//a pascal string has the length in hex first, followed by the string in hex  
+const
+  pascalified = hexnum + hexxedAndReturned
 
-  console.log("answer is " + hexxedAndReturned)
-  //return hexxed
-
-
+  console.log(pascalified)
+  return pascalified
 

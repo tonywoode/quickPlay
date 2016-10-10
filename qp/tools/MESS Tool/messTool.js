867ac61c4716745c0d580d11cb61dc62ef02c6f1
tony woode <tonywoodecode@gmail.com>
2016-10-10 08:04:29 +0100

    writing new systems dat to output dir

--- qp/tools/MESS Tool/messTool.js
+++ qp/tools/MESS Tool/messTool.js
@@ -6,29 +6,51 @@ const fs        = require('fs')
 
 const stream = fs.createReadStream("inputs/mame.xml")
   , xml = new XmlStream(stream)
+  , systems = []
+
 
 let prev
   , system
 
+makeArray(printArrayToFile)
 
-xml.on("updateElement: machine", function(machine) {
-  const attr = machine.$.name
-  if (machine.softwarelist 
-     && attr !== prev 
-     && machine.driver.$.emulation === "good"
-     && !machine.$.cloneof
-  ) {
-    //don't want systems with two manufacturers names eg atari atari 2600
-    const company = machine.manufacturer
-    const systemName = machine.description
-    const separator = " "
-    const numberOfWords = 1
-    const firstWordOfCompany = company.split(separator, numberOfWords)
-    if (systemName.indexOf(firstWordOfCompany) === 0 || company === "<unknown>") { system = systemName}
-    else { system = company + " " + machine.description }
 
-    console.log(system + " aka " + attr + " has at least one softlist, its a good system, and its not a clone")
-    prev = attr
-  }
-});
 
+function makeArray(callback){
+
+  xml.on("updateElement: machine", function(machine) {
+    const attr = machine.$.name
+    if (machine.softwarelist 
+       && attr !== prev 
+       && machine.driver.$.emulation === "good"
+       && !machine.$.cloneof
+    ) {
+      //don't want systems with two manufacturers names eg atari atari 2600
+      const company = machine.manufacturer
+        , systemName = machine.description
+        , separator = " "
+        ,  numberOfWords = 1
+        ,  firstWordOfCompany = company.split(separator, numberOfWords)
+      if (systemName.indexOf(firstWordOfCompany) === 0 || company === "<unknown>") { system = systemName}
+      else { system = company + " " + machine.description }
+      systems.push(system)
+     // console.log(system + " aka " + attr + " has at least one softlist, its a good system, and its not a clone")
+      prev = attr
+    }
+  })
+
+  xml.on("end", function(){
+  callback(systems)
+  })
+}
+
+function printArrayToFile(array){
+const opPath = ("outputs/newsystems.dat")
+const output = fs.createWriteStream(opPath)
+const newsystems = array.sort()
+output.on('error', function(err) { console.log("couldn't write the file") });
+
+//console.log(newsystems.toString());
+newsystems.forEach(function(v) { output.write(v + '\n'); });
+output.end();
+}

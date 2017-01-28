923d450a2bf6e3486bf374e6e44ee384b882df1a
tony woode <tonywoodecode@gmail.com>
2017-01-28 20:03:01 +0000

    more converted replacements

--- qp/tools/MESS Tool/messTool.js
+++ qp/tools/MESS Tool/messTool.js
@@ -61,6 +61,7 @@ function sanitise(systems, callback){
   //replacement functions
   , companyReplace = (oldCompany, newCompany)            => R.map( ( {company, system } ) => ( {company: company.replace(oldCompany, newCompany),system}))
   , systemReplace  = (thisCompany, oldsystem, newsystem) => R.map( ( {company, system } ) => ( {company, system: (company.match(thisCompany) && system.match(oldsystem))? newsystem : system}))
+
   //transforms  
   , res = R.pipe(
   //general rules
@@ -70,7 +71,7 @@ function sanitise(systems, callback){
   , R.map( ( {company, system } ) => ( {company: system.match(/MSX2/)? '' : company, system: system.match(/MSX2/)? `MSX2` : system})) 
   , R.map( ( {company, system } ) => ( {company, system: system.replace(/(\(.*\)|\(.*\))/, ``)})) //now MSX has gone, every bracketed item is unnecessary
 
-    //system specific (btw replace accepts a regex by default, but i'm trying to show what's a string and what's a regex)
+    //system specific (btw replace accepts regex or string by default, but i'm trying to show what's intended as a string and what's a regex)
   , systemReplace(`Acorn`, /BBC/, `BBC`), systemReplace(`Acorn`, /Electron/, `Atom`)
   , companyReplace(/Amstrad .*/, `Amstrad`), systemReplace(`Amstrad`, /(CPC|GX4000)/, `CPC`)
   , companyReplace(/Apple Computer/, `Apple`), systemReplace(`Apple`, /Macintosh /, ``), systemReplace(`Apple`,/II.*/,`II`) 
@@ -78,30 +79,37 @@ function sanitise(systems, callback){
   , companyReplace("Bally Manufacturing","Bally")
   , systemReplace(`Bandai`,`Super Vision 8000`, `Super Vision`) 
   , systemReplace(`Bondwell Holding`, `.*`, `Bondwell`), companyReplace(`Bondwell Holding`, ``)
-  , R.map( ( {company, system } ) => ( {company: company.replace(`Commodore Business Machines`, `Commodore`),system}))
-  , R.map( ( {company, system } ) => ( {company, system:  (company.match(`Commodore`) && system.match(/B500|P500/))? `500/600/700` : system}))
-  , R.map( ( {company, system } ) => ( {company, system:  (company.match(`Commodore`) && system.match(/PET .*|CBM .*/))? `PET/CBM` : system}))
-  , R.map( ( {company, system } ) => ( {company, system:  (company.match(`Commodore`) && system.match(/64|128/))? `64/128` : system}))
-  , R.map( ( {company, system } ) => ( {company, system:  (company.match(`Commodore`) && system.match(`VIC-10 / Max Machine / UltiMax`))? `Max/Ultimax`: system}))
-  , R.map( ( {company, system } ) => ( {company, system:  (company.match(`EACA`) && system.match(`Colour Genie EG2000`))? `Colour Genie`: system}))
-  , R.map( ( {company, system } ) => ( {company, system:  (company.match(`Elektronika`) && system.match(`BK 0010`))? `BK`: system}))
-  , R.map( ( {company, system } ) => ( {company, system:  (company.match(`Emerson`) && system.match(`Arcadia 2001`))? `Arcadia`: system}))
-  , R.map( ( {company, system } ) => ( {company, system:  (company.match(`Epson`) && system.match(`PX-4`))? `PX`: system}))
-  , R.map( ( {company, system } ) => ( {company: company.replace(`Exidy Inc`, `Exidy`),system}))
-  , R.map( ( {company, system } ) => ( {company, system:  (company.match(`Fairchild`) && system.match(`Channel F II`))? `Channel F`: system}))
-  , R.map( ( {company, system } ) => ( {company, system:  (company.match(`Fujitsu`) && system.match(`FM-7`))? `Micro 7`: system}))
-  , R.map( ( {company, system } ) => ( {company: company.replace(`General Consumer Electronics`, `GCE`),system}))
-  , R.map( ( {company, system } ) => ( {company: company.replace(`Interton Electronic`, `Interton`), system: (company.match(`Interton`) && system.match(`VC 4000`))? `VC4000` : system}))
-  , R.map( ( {company, system } ) => ( {company: company.replace(`Jupiter Cantab`, `Jupiter`),system}))
-  , R.map( ( {company, system } ) => ( {company, system: (company.match(`Kyosei`) && system.match(`Kyotronic 85`))? `Kyotronic` : system}))
+  , companyReplace(`Commodore Business Machines`, `Commodore`), systemReplace(`Commodore`, /B500|P500/, `500/600/700`) 
+    , systemReplace(`Commodore`, /PET .*|CBM .*/, `PET/CBM`), systemReplace(`Commodore`, /64|128/, `64/128`)
+    , systemReplace(`Commodore`, `VIC-10 / Max Machine / UltiMax`, `Max/Ultimax`) 
+  , systemReplace(`EACA`,`Colour Genie EG2000`, `Colour Genie`)
+  , systemReplace(`Elektronika`,`BK 0010`, `BK`)
+  , systemReplace(`Emerson`, `Arcadia 2001`, `Arcadia`)
+  , systemReplace(`Epson`, `PX-4`, `PX`)
+  , companyReplace(`Exidy Inc`, `Exidy`)
+  , systemReplace(`Fairchild`, `Channel F II`, `Channel F`)
+  , systemReplace(`Fujitsu`, `FM-7`, `Micro 7`)
+  , companyReplace(`General Consumer Electronics`, `GCE`)
+  , companyReplace(`Interton Electronic`, `Interton`)
+  , companyReplace(`Jupiter Cantab`, `Jupiter`)
+  , systemReplace(`Kyosei`, `Kyotronic 85`, `Kyotronic`)
+
   , R.map( ( {company, system } ) => ( {company: company.replace(`Luxor Datorer AB`, `Luxor`),system:  (company.match(`Luxor`) && system.match(/ABC.*/))? `ABC`: system}))
+
   , R.map( ( {company, system } ) => ( {company: company.replace(`Matra & Hachette`, `Tandy Radio Shack`), system: (company.match(`Matra & Hachette`) && system.match(`Alice 32`))? `MC-10` : system}))
+
   , R.map( ( {company, system } ) => ( {company: company.replace(`Memotech Ltd`, `Memotech`), system: (company.match(`Memotech`) && system.match(`MTX .*`))? `MTX` : system}))
+
   , R.map( ( {company, system } ) => ( {company, system:  (company.match(`Mikroelektronica`) && system.match(`Pyldin-601`))? `Pyldin`: system}))
+
   , R.map( ( {company, system } ) => ( {company: company.replace(`Nascom Microcomputers`, ``), system: (company.match(``) && system.match(/^2$/)? `Nascom` : system)}))
+
   , R.map( ( {company, system } ) => ( {company, system:  (company.match(`Nintendo`) && system.match(`Entertainment System / Famicom`))? `NES`: system}))
+
   , R.map( ( {company, system } ) => ( {company, system:  (company.match(`Nintendo`) && system.match(`Game Boy Color`))? `Game Boy`: system}))
+
   , R.map( ( {company, system } ) => ( {company, system:  (company.match(`Nintendo`) && system.match(`Super Entertainment System / Super Famicom `))? `SNES`: system}))
+
   , R.map( ( {company, system } ) => ( {company: company.replace(`Nippon Electronic Company`, `NEC`), system: system.match(`PC Engine`)? `PC Engine\\TurboGrafx-16` : system}))
 
   // lastly dedupe all the dupes we just made in those transforms

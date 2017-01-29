d9187bf5bb428f89daf0256315004571f693e2d9
tony woode <tonywoodecode@gmail.com>
2017-01-29 19:48:54 +0000

    finished 1st run of sys/comp replacements

--- qp/tools/MESS Tool/messTool.js
+++ qp/tools/MESS Tool/messTool.js
@@ -59,68 +59,79 @@ function sanitise(systems, callback){
   , numberOfWords = 1
   
   //replacement functions
-  , companyReplace = (oldCompany, newCompany)            => R.map( ( {company, system } ) => ( {company: company.replace(oldCompany, newCompany),system}))
-  , systemReplace  = (thisCompany, oldsystem, newsystem) => R.map( ( {company, system } ) => ( {company, system: (company.match(thisCompany) && system.match(oldsystem))? newsystem : system}))
+  , compRep = (oldCompany, newCompany)            => R.map( ( {company, system } ) => ( {company: company.replace(oldCompany, newCompany),system}))
+  , systRep = (thisCompany, oldsystem, newsystem) => R.map( ( {company, system } ) => ( {company, system: (company.match(thisCompany) && system.match(oldsystem))? newsystem : system}))
 
   //transforms  
   , res = R.pipe(
   //general rules
-    companyReplace(`<unknown>`, ``)
+    compRep(`<unknown>`, ``)
   , R.map( ( {company, system } ) => ( {company, system: system.replace(new RegExp(company.split(separator, numberOfWords) + '\\W', "i"), "")} )) //take company from system name if they repeat
   , R.map( ( {company, system } ) => ( {company: system.match(/MSX1/)? '' : company, system: system.match(/MSX1/)? `MSX` : system}))
   , R.map( ( {company, system } ) => ( {company: system.match(/MSX2/)? '' : company, system: system.match(/MSX2/)? `MSX2` : system})) 
   , R.map( ( {company, system } ) => ( {company, system: system.replace(/(\(.*\)|\(.*\))/, ``)})) //now MSX has gone, every bracketed item is unnecessary
 
     //system specific (btw replace accepts regex or string by default (i'm trying to show what's intended), but match matches only regex
-  , systemReplace(`Acorn`, /BBC/, `BBC`), systemReplace(`Acorn`, /Electron/, `Atom`)
-  , companyReplace(/Amstrad .*/, `Amstrad`), systemReplace(`Amstrad`, /(CPC|GX4000)/, `CPC`)
-  , companyReplace(`APF Electronics Inc.`, `APF`), systemReplace(`APF`, `M-1000`, `Imagination Machine`)
-  , companyReplace(/Apple Computer/, `Apple`), systemReplace(`Apple`, /(Macintosh LC|Macintosh II.*)/, `Macintosh II (68020/68030)`)
-    , systemReplace(`Apple`, /Macintosh (Plus|SE|Classic)/, `Macintosh (6800)`), systemReplace(`Apple`,/(^II.*|\]\[|\/\/c|\/\/e)/,`II`)
-    , systemReplace(`Apple`,/\/\/\//,`III`)
-  , systemReplace(`Atari`,/(400|^800.*|XE Game System)/, `400/600/800/1200/XE`)
-  , companyReplace("Bally Manufacturing","Bally")
-  , systemReplace(`Bandai`,`Super Vision 8000`, `Super Vision`) 
-  , systemReplace(`Bondwell Holding`, `.*`, `Bondwell`), companyReplace(`Bondwell Holding`, ``) //change company after
-  , companyReplace(`Commodore Business Machines`, `Commodore`), systemReplace(`Commodore`, /B500|P500/, `500/600/700`) 
-    , systemReplace(`Commodore`, /PET .*|CBM .*/, `PET/CBM`), systemReplace(`Commodore`, /64|128/, `64/128`)
-    , systemReplace(`Commodore`, `VIC-10 / Max Machine / UltiMax`, `Max/Ultimax`), systemReplace(`Commodore`, `VIC-1001`, `VIC-20`) 
-  , systemReplace(`EACA`,`Colour Genie EG2000`, `Colour Genie`)
-  , systemReplace(`Elektronika`,`BK 0010`, `BK`)
-  , systemReplace(`Emerson`, `Arcadia 2001`, `Arcadia`)
-  , systemReplace(`Epson`, `PX-4`, `PX`)
-  , companyReplace(`Exidy Inc`, `Exidy`)
-  , systemReplace(`Fairchild`, `Channel F II`, `Channel F`)
-  , systemReplace(`Fujitsu`, `FM-7`, `Micro 7`)
-  , companyReplace(`General Consumer Electronics`, `GCE`)
-  , companyReplace(`Interton Electronic`, `Interton`)
-  , companyReplace(`Jupiter Cantab`, `Jupiter`)
-  , systemReplace(`Kyosei`, `Kyotronic 85`, `Kyotronic`)
-  , companyReplace(`Luxor Datorer AB`, `Luxor`), systemReplace(`Luxor`, /ABC.*/, `ABC`)
-  , systemReplace(`Matra & Hachette`, `Alice 32`, `MC-10`), companyReplace(`Matra & Hachette`, `Tandy Radio Shack`) //change company after
-  , companyReplace(`Memotech Ltd`, `Memotech`), systemReplace(`Memotech`, `MTX .*`, `MTX`) 
-  , systemReplace(`Mikroelektronika`, `Pyldin-601`, `Pyldin`)
-  , systemReplace(`Nascom Microcomputers`, `2`, `Nascom`), companyReplace(`Nascom Microcomputers`, ``) //change company after
-  , systemReplace(`Nintendo`, `Entertainment System / Famicom`, `NES`)
-  , systemReplace(`Nintendo`, `Game Boy Color`, `Game Boy`)
-  , systemReplace(`Nintendo`, `Super Entertainment System / Super Famicom `, `SNES`)
-  , companyReplace(`Nippon Electronic Company`, `NEC`), systemReplace(`NEC`, `PC Engine`, `PC Engine/TurboGrafx-16`)
-  , systemReplace(`Non Linear Systems`, `Kaypro II - 2/83`, `Kaypro`)
-  , companyReplace(`Data Applications International`, `DAI`), systemReplace(`DAI`, `DAI Personal Computer`, `Personal Computer`)
-  , companyReplace(`Elektronika inzenjering` , ``)
-  , systemReplace(`International Business Machines`, `IBM PC 5150`, `PC 5150`), companyReplace(`International Business Machines`, `IBM`) //change company after
-  , systemReplace(`Interton`, `Electronic VC 4000` , `VC 4000`)
-  , systemReplace(``, `Orion128` , `Orion`) //note these assume youve transformed <unknown> already
-  , systemReplace(``, `PK8020Korvet` , `Korvet PK`)
-  , companyReplace(`Jungle Soft / KenSingTon / Chintendo / Siatronics`, '')
-  , systemReplace(/Welback Holdings .*/ , `Mega Duck / Cougar Boy`, `Mega Duck/Couger Boy`), companyReplace(/Welback Holdings .*/, ``) //change company after
-  , companyReplace(`Miles Gordon Technology plc`, `MGT`)
-  , companyReplace(`Processor Technology Corporation`, `PTC`), systemReplace(`PTC`, `SOL-20`, `Sol`)
-  , systemReplace(``, `Radio86RK` , `Radio-86RK`) //seems MESS made the mistake here...
-  , systemReplace(`Sanyo`, `MBC-55x`, `MBC`)
-  , systemReplace(`SNK`, /(Neo-Geo$|Neo-Geo AES)/, `Neo Geo`), systemReplace(`SNK`, `Neo-Geo CDZ`, `Neo Geo CD`) //wikipedia says mess is wrong
-    , systemReplace(`SNK`, `NeoGeo Pocket`, `Neo Geo Pocket`) //mess says mess is wrong....
-
+  , systRep(`Acorn`, /BBC/, `BBC`), systRep(`Acorn`, /Electron/, `Atom`)
+  , compRep(/Amstrad .*/, `Amstrad`), systRep(`Amstrad`, /(CPC|GX4000)/, `CPC`)
+  , compRep(`APF Electronics Inc.`, `APF`), systRep(`APF`, `M-1000`, `Imagination Machine`)
+  , compRep(/Apple Computer/, `Apple`), systRep(`Apple`, /(Macintosh LC|Macintosh II.*)/, `Macintosh II (68020/68030)`)
+    , systRep(`Apple`, /Macintosh (Plus|SE|Classic)/, `Macintosh (6800)`), systRep(`Apple`,/(^II.*|\]\[|\/\/c|\/\/e)/,`II`)
+    , systRep(`Apple`,/\/\/\//,`III`)
+  , systRep(`Atari`,/(400|^800.*|XE Game System)/, `400/600/800/1200/XE`)
+  , compRep(`Bally Manufacturing`,`Bally`)
+  , systRep(`Bandai`,`Super Vision 8000`, `Super Vision`) 
+  , systRep(`Bondwell Holding`, /.*/, `Bondwell`), compRep(`Bondwell Holding`, ``) //change company after
+  , compRep(`Commodore Business Machines`, `Commodore`), systRep(`Commodore`, /B500|P500/, `500/600/700`) 
+    , systRep(`Commodore`, /PET .*|CBM .*/, `PET/CBM`), systRep(`Commodore`, /64|128/, `64/128`)
+    , systRep(`Commodore`, `VIC-10 / Max Machine / UltiMax`, `Max/Ultimax`), systRep(`Commodore`, `VIC-1001`, `VIC-20`) 
+  , systRep(`EACA`,`Colour Genie EG2000`, `Colour Genie`)
+  , systRep(`Elektronika`,`BK 0010`, `BK`)
+  , systRep(`Emerson`, `Arcadia 2001`, `Arcadia`)
+  , systRep(`Epson`, `PX-4`, `PX`)
+  , compRep(`Exidy Inc`, `Exidy`)
+  , systRep(`Fairchild`, `Channel F II`, `Channel F`)
+  , systRep(`Fujitsu`, `FM-7`, `Micro 7`)
+  , compRep(`General Consumer Electronics`, `GCE`)
+  , compRep(`Interton Electronic`, `Interton`)
+  , compRep(`Jupiter Cantab`, `Jupiter`)
+  , systRep(`Kyosei`, `Kyotronic 85`, `Kyotronic`)
+  , compRep(`Luxor Datorer AB`, `Luxor`), systRep(`Luxor`, /ABC.*/, `ABC`)
+  , systRep(`Matra & Hachette`, `Alice 32`, `MC-10`), compRep(`Matra & Hachette`, `Tandy Radio Shack`) //change company after
+  , compRep(`Memotech Ltd`, `Memotech`), systRep(`Memotech`, `MTX .*`, `MTX`) 
+  , systRep(`Mikroelektronika`, `Pyldin-601`, `Pyldin`)
+  , systRep(`Nascom Microcomputers`, `2`, `Nascom`), compRep(`Nascom Microcomputers`, ``) //change company after
+  , systRep(`Nintendo`, `Entertainment System / Famicom`, `NES`)
+  , systRep(`Nintendo`, `Game Boy Color`, `Game Boy`)
+  , systRep(`Nintendo`, `Super Entertainment System / Super Famicom `, `SNES`)
+  , compRep(`Nippon Electronic Company`, `NEC`), systRep(`NEC`, `PC Engine`, `PC Engine/TurboGrafx-16`)
+  , systRep(`Non Linear Systems`, `Kaypro II - 2/83`, `Kaypro`)
+  , compRep(`Data Applications International`, `DAI`), systRep(`DAI`, `DAI Personal Computer`, `Personal Computer`)
+  , compRep(`Elektronika inzenjering` , ``)
+  , systRep(`International Business Machines`, `IBM PC 5150`, `PC 5150`), compRep(`International Business Machines`, `IBM`) //change company after
+  , systRep(`Interton`, `Electronic VC 4000` , `VC 4000`)
+  , systRep(``, `Orion128` , `Orion`) //note these assume youve transformed <unknown> already
+  , systRep(``, `PK8020Korvet` , `Korvet PK`)
+  , compRep(`Jungle Soft / KenSingTon / Chintendo / Siatronics`, '')
+  , systRep(/Welback Holdings .*/ , `Mega Duck / Cougar Boy`, `Mega Duck/Couger Boy`), compRep(/Welback Holdings .*/, ``) //change company after
+  , compRep(`Miles Gordon Technology plc`, `MGT`)
+  , compRep(`Processor Technology Corporation`, `PTC`), systRep(`PTC`, `SOL-20`, `Sol`)
+  , systRep(``, `Radio86RK` , `Radio-86RK`) //seems MESS made the mistake here...
+  , systRep(`Sanyo`, `MBC-55x`, `MBC`)
+  , systRep(`SNK`, /(Neo-Geo$|Neo-Geo AES)/, `Neo Geo`), systRep(`SNK`, `Neo-Geo CDZ`, `Neo Geo CD`) //wikipedia says MESS is wrong
+    , systRep(`SNK`, `NeoGeo Pocket`, `Neo Geo Pocket`) //MESS says MESS is wrong....
+  , systRep(`Sega`, `Genesis`, `Genesis/32X`), systRep(`Sega`, `Master System II`, `Master System`)
+     , systRep(`Sega`, /(SC-3000|SG-1000)/, `SG-1000/SC-3000/SF-7000`)
+  , systRep(`Sharp`, /MZ.*/, `MZ`)
+  , compRep(`Sinclair Research Ltd`, `Sinclair`), systRep(`Sinclair`, /ZX Spectrum .*/, `ZX Spectrum`)
+  , systRep(`Sord`, `m.5`, `M5`)
+  , systRep(`Spectravideo`, `SVI-318`, `SVI`)
+  , systRep(`Tandy Radio Shack`, /(TRS-80 .*|Color Computer)/, `TRS-80 CoCo`)
+  , systRep(`Texas Instruments`, /TI-99.*/, 'TI-99')
+  , systRep(`Thomson`, `MO5 NR`, `MO5`), systRep(`Thomson`, /(TO7.*|TO9.*)/, `TO7/TO9`)
+  , compRep(`V. I. Lenin`, `Lenin`), systRep(`Lenin`, `PK-01 Lviv`, `Lviv`)
+  , systRep(`Video Technology`, /Laser.*/, `Laser Mk1`)
+  , systRep(`Watara`, `Super Vision`, `Supervision`) //again MESS seems to be wrong
 
   // lastly dedupe all the dupes we just made in those transforms
   , R.uniq //so in anything above, we can duplicate to become unique....

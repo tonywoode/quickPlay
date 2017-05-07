cecb9d1270ee637f6084af39cd7f3b06077b6aca
tony woode <tonywoodecode@gmail.com>
2017-05-07 07:45:07 +0100

    DO add trivial sourcecode for movers

---
+++ qp/tools/Mame Softlists/getMAMESoftlists.js
@@ -0,0 +1,2 @@
+const fs = require('fs')
+fs.renameSync(`mame_softlists`, `../../data/MAME Softlists`)
---
+++ qp/tools/RetroArch Softlists/getRetroarchSoftlists.js
@@ -0,0 +1,2 @@
+const fs = require('fs')
+fs.renameSync(`retroarch_softlists`, `../../data/RetroArch Softlists`)
